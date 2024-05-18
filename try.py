14b0c1bf602f9652680e3d646c7abad485348388
ermolaevaalenaandreevna <alyonaerm13@gmail.com>
2024-05-18 13:05:53 +1000

    переключение между страницами

--- try.py
+++ try.py
@@ -43,7 +43,7 @@ class MainWindow(QMainWindow):
         data = self.cur.fetchall()
 
         for i, row in enumerate(data):
-            widget = MyWidget('images/futbolka.png', row[1], row[2])
+            widget = MyWidget(f'{row[3]}', row[1], row[2])
             self.gridLayout.addWidget(widget, 0, i)
 
         self.page = 0
@@ -62,8 +62,8 @@ class MainWindow(QMainWindow):
         data = self.cur.fetchall()
 
         for i, row in enumerate(data):
-            widget = MyWidget('images/dress1.jpg', row[1],row[2])
-            self.gridLayout.addWidget(widget, 0, i)
+            widget = MyWidget(f'{row[3]}', row[1],row[2])
+            self.gridLayout.addWidget(widget, 0, 4-i)
 
 if __name__ == '__main__':
     import sys
